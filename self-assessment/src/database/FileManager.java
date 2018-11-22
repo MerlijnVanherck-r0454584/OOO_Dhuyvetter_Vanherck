24e4fe9a723324707eb8bfcc302a77ccb4df3452
Merlijn Vanherck <merlijn.vanherck@student.ucll.be>
2018-11-22 14:53:39 +0100

    created file manager class and empty db controller

--- self-assessment/src/database/FileManager.java
+++ self-assessment/src/database/FileManager.java
@@ -1,6 +1,32 @@
 package database;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Scanner;
+
 public class FileManager {
 	
-	public readFromFile();
+	public static String readEntireFile(String fileName) throws FileNotFoundException {
+		Scanner fileReader = new Scanner(new File(fileName));
+		String fileText = "";
+		
+		while(fileReader.hasNextLine())
+		{
+		   fileText = fileReader.nextLine();
+		}
+		
+		fileReader.close();
+		return fileText;
+	}
+	
+	public static void writeToFile(String fileName, String textToWrite) throws IOException {
+		Writer fileWriter = new FileWriter(fileName, true);
+		
+		fileWriter.write(textToWrite);
+		
+		fileWriter.close();
+	}
 }
